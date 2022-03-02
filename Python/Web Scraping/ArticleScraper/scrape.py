09251189ac455d1562226496f2639c742522f291
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2022-03-02 17:16:17 +0530

    Added Article Scraper Script

---
+++ Python/Web Scraping/ArticleScraper/scrape.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+
+def scrape_pages(url):
+    '''
+    For scraping article and getting its title and body(p tags content)
+    '''
+    try:
+        res = requests.get(url)
+
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.content, 'html.parser')
+
+            title = soup.title.text
+
+            p_content = soup.findAll('p')
+            p_content = " ".join([p.text for p in p_content])
+
+    except:
+        print("Some exception occured.")
+
+    return title, p_content
+
+
+if __name__ == '__main__':
+
+    for url in sys.argv[1:]:
+        title, body = scrape_pages(url)
+        print(title)
+        print(body)
+
+
+
+
+#***********************  Made By: Vishesh  ************************ #
+#*************** https://github.com/visheshks04 *********************#
