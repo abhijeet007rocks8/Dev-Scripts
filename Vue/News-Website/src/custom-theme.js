03de8313b228b76c86a23fd1be058c689f664b34
Jyo561 <76492740+Jyo561@users.noreply.github.com>
2022-03-06 02:23:42 +0530

    Add files via upload

---
+++ Vue/News-Website/src/custom-theme.js
@@ -0,0 +1,22 @@
+//  custom-theme.js
+export default {
+  breakpoints: ['30em', '48em', '62em', '80em'],
+  fonts: {
+    body: 'Fredoka, sans-serif',
+    
+  },
+  colors: {
+    brand: {
+      50: '#daffff',
+      100: '#b1fbfb',
+      200: '#85f7f7',
+      300: '#58f3f3',
+      400: '#31f0f0',
+      500: '#1ed7d7',
+      600: '#0ca7a7',
+      700: '#007777',
+      800: '#004949',
+      900: '#001a1a',
+    }
+  },
+}
