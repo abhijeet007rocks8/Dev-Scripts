78eb195950815e70de7d246619b6f4838a12a0ef
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2022-03-23 15:44:41 +0530

    fixed potential deprecations

--- Python/Automations/WhatsappBotForTimedTexts/spammer.py
+++ Python/Automations/WhatsappBotForTimedTexts/spammer.py
@@ -1,12 +1,17 @@
 import sys
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import warnings
 
 warnings.filterwarnings('ignore')
 
-driver = webdriver.Chrome(executable_path='Python/Automations/WhatsappBotForTimedTexts/chromedriver')
+s=Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+# driver = webdriver.Chrome(executable_path='Python/Automations/WhatsappBotForTimedTexts/chromedriver')
 driver.get("https://web.whatsapp.com")
 
 def spam(message, times):
@@ -16,14 +21,20 @@ def spam(message, times):
         times - time stamps to spam on
     '''
     textfield_xpath = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]'
-    text_field = driver.find_element_by_xpath(textfield_xpath)
+    
+    try:
+        text_field = driver.find_element(by=By.XPATH, value=textfield_xpath)
 
-    for duration in times:
-        time.sleep(duration)
-        text_field.send_keys(message)
-        text_field.send_keys(Keys.ENTER)
+        for duration in times:
+            time.sleep(duration)
+            text_field.send_keys(message)
+            text_field.send_keys(Keys.ENTER)
 
-input('At this moment, You must log into your whatsapp account and open up the inbox to spam. Then press any key.')
+    except:
+        input('Something is not right. Check if you have logged in successfully or if you are inside an inbox. Then press any key.')
+        spam(message, times)
+
+input('\n\nAt this moment, You must log into your whatsapp account and open up the inbox to spam. Then press any key.')
 
 message = sys.argv[1]
 times = list(map(int, sys.argv[2:]))
