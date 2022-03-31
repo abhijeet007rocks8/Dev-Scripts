16120fa7edd57c047a83bc807b3e3f98289d98aa
Sidharth Sarangi <78564629+SidharthSarangi@users.noreply.github.com>
2022-03-31 09:19:57 +0530

    Added extra features to pr #218 (#221)
    * Added Delete.js
    
    * updated the sample video

--- React/student data render/info/src/Show.js
+++ React/student data render/info/src/Show.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const Show = () => {
+const Show = (props) => {
+    const {n} = props ;
     const list1 = [];
     const list2 = [];
     const list3 = [];
     const list4 = [];
     const list5 = []; 
 
-    const n = 1;
+    //const n = 1;
     const x = 0;
 
     const addRown = document.getElementById('show');
@@ -19,20 +20,18 @@ const Show = () => {
     list4[x] = document.getElementById("skills").value;
     list5[x] = document.getElementById("email").value;
 
-    const cel1 = NewRow.insertCell(0);
-    const cel2 = NewRow.insertCell(1);
-    const cel3 = NewRow.insertCell(2);
-    const cel4 = NewRow.insertCell(3);
-    const cel5 = NewRow.insertCell(4) ;
+    const cell1 = NewRow.insertCell(0);
+    const cell2 = NewRow.insertCell(1);
+    const cell3 = NewRow.insertCell(2);
+    const cell4 = NewRow.insertCell(3);
+    const cell5 = NewRow.insertCell(4) ;
 
-    cel1.innerHTML = list1[x];
-    cel2.innerHTML = list2[x];
-    cel3.innerHTML = list3[x];
-    cel4.innerHTML = list4[x];
-    cel5.innerHTML = list5[x] ;
+    cell1.innerHTML = list1[x];
+    cell2.innerHTML = list2[x];
+    cell3.innerHTML = list3[x];
+    cell4.innerHTML = list4[x];
+    cell5.innerHTML = list5[x] ;
 
-    n++;
-    x++;
 //   return (
 //     <table className="table-class">
 //         <tr>
