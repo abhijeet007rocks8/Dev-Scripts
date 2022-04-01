fb8f78722f0303cc0a7db867ae852222d2f52f8a
jeevanalok <inventorjeevan@gmail.com>
2022-04-01 22:40:58 +0530

    removed comments

--- Python/Send_Supplies_Game/game.py
+++ Python/Send_Supplies_Game/game.py
@@ -1,29 +1,3 @@
-"""
-<---Story---> 
-Lieutenant John Fowl is admist a war torn area.His troops need supplies ASAP and 
-there is no time to waste.He makes his way to different strategic places to receive the
-supplies. By sheer will and determination, he makes his way to the rendezvous point.
-Just as he was taking a quick breath, a bzzz sound is heard and what appears is a plane 
-flying towards him. John's relived face soon turns into horror as he realizes discovers the
-package dropped by plane, is a BOMB!. He then checks back his intel, and finds out that their cover
-is blown.Enemy planes are disguised as friendly with not so friendly intention. John's team
-is in dire need of supplies and a new plan will take 3 months minimum
-Gear up as you are the last hope of lieutenant
-
-
-TASK:- Help Lt.John Fowl to receive the packages from the allies' plane and beware
-		of the enemy plane  and DON'T DIE
-
-This simple game is made by pygame, a game library written in python language
-Play through and feel free to fork it and add some addition of your own
-
-Godspeed Reader!
-"""
-
-
-
-
-
 
 #importing necessary libraries
 import pygame
