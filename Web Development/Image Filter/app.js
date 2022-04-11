c4b047a98780a533f3aa0804bed899aabc226a9f
iamrahulmahato <rahul25mahato@gmail.com>
2022-04-11 08:48:47 -0700

    :memo: var created

---
+++ Web Development/Image Filter/app.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let img = new Image();
+let fileName = "";
+
+const downloadBtn = document.getElementById("download-btn");
+const uploadFile = document.getElementById("upload-file");
+const revertBtn = document.getElementById("revert-btn");
