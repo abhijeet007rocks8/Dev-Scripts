54c98600b4e26bc490466f98690db8c092d21a94
Sarthak S Kumar <sskworld9742@gmail.com>
2022-05-01 10:56:40 +0530

    Changes in Scripts, Added Assets

--- Python/Zoom_Automation/script.py
+++ Python/Zoom_Automation/script.py
@@ -6,7 +6,7 @@ PASSCODE = input("Enter Meeting password: ")
 DURATION = input("Enter total duration of meet in seconds")
 
 
-def autozoom():
+def start():
     # opening zoom app
     pygui.hotkey("alt", "f2")
     clock.sleep(5)
@@ -18,20 +18,24 @@ def autozoom():
     x_c, y_c = pygui.locateCenterOnScreen("img/join.png", confidence=0.9)
     clock.sleep(5)
     pygui.click(x_c, y_c)
+
     # adding ID
     clock.sleep(5)
     x_s, y_s = pygui.locateCenterOnScreen("img/s3.png", confidence=0.9)
     pygui.click(x_s, y_s)
     pygui.write(ID)
+
     # video off
     clock.sleep(5)
     x_s, y_s = pygui.locateCenterOnScreen("img/s2.png", confidence=0.9)
     pygui.click(x_s, y_s)
+
     # audio off
     clock.sleep(5)
     x_s, y_s = pygui.locateCenterOnScreen("img/s1.png", confidence=0.9)
     pygui.click(x_s, y_s)
     pygui.press("enter", interval=5)
+
     # entering a passcode
     pygui.write(PASSCODE)
     pygui.press("enter", interval=10)
@@ -46,4 +50,4 @@ def autozoom():
     pygui.hotkey("alt", "f4")
 
 
-autozoom()
+start()
